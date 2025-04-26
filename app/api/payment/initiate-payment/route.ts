9a3c413213cc15b1419e8449a4c18025463acb37
Belrick Stephane <73307766+latioms@users.noreply.github.com>
2025-04-26 01:17:03 +0100

    feat(payment): refactor authentication and payload construction for payment initiation

--- app/api/payment/initiate-payment/route.ts
+++ app/api/payment/initiate-payment/route.ts
@@ -2,30 +2,30 @@ import { NextRequest, NextResponse } from "next/server";
 import { randomUUID } from "crypto";
 
 async function getAuthToken() {
-    try {
-        const response = await fetch(
-          `${process.env.EJARA_MOMO_PAYMENT_SANDBOX_URL}/api/v1/accounts/authenticate`,
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              "client-secret": process.env.EJARA_MOMO_CLIENT_SECRET!,
-              "client-key": process.env.EJARA_MOMO_CLIENT_KEY!,
-            },
-          }
-        );
-    
-        const data = await response.json();
-        console.log("Authentication response:", data);
-    
-        return data.data;
-    
-      } catch (error) {
-        return NextResponse.json(
-          { error: "Failed to authenticate" },
-          { status: 500 }
-        );
+  try {
+    const response = await fetch(
+      `${process.env.EJARA_MOMO_PAYMENT_SANDBOX_URL}/api/v1/accounts/authenticate`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "client-secret": process.env.EJARA_MOMO_CLIENT_SECRET!,
+          "client-key": process.env.EJARA_MOMO_CLIENT_KEY!,
+        },
       }
+    );
+
+    const data = await response.json();
+    console.log("Authentication response:", data);
+
+    return data.data;
+
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to authenticate" },
+      { status: 500 }
+    );
+  }
 }
 
 export async function POST(request: NextRequest) {
@@ -42,18 +42,6 @@ export async function POST(request: NextRequest) {
     console.log("Token:", token);
 
     const body = await request.json();
-    const payload = {
-      phoneNumber: body.phoneNumber,
-      transactionType: "payin",
-      amount: body.amount as string,
-      fullName: body.fullName,
-      emailAddress: body.emailAddress,
-      currencyCode: body.currencyCode || "XAF",
-      countryCode: body.countryCode || "CM",
-      paymentMode: body.paymentMode || "OM",
-      externalReference: body.externalReference || randomUUID(),
-      featureCode: "PRO"
-    };
 
     const response = await fetch(
       `${sandbox_url}/api/v1/transactions/initiate-momo-payment`,
@@ -61,12 +49,23 @@ export async function POST(request: NextRequest) {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          "accept": "application/json",
           "Authorization": `Bearer ${token}`,
           "client-secret": process.env.EJARA_MOMO_CLIENT_SECRET!,
           "client-key": process.env.EJARA_MOMO_CLIENT_KEY!,
+          ""
         },
-        body: JSON.stringify(payload),
+        body: JSON.stringify({
+          phoneNumber: body.phoneNumber,
+          transactionType: "payin",
+          amount: body.amount,
+          fullName: body.fullName,
+          emailAddress: body.emailAddress,
+          currencyCode: body.currencyCode || "XAF",
+          countryCode: body.countryCode || "CM",
+          paymentMode: body.paymentMode || "OM",
+          externalReference: randomUUID(),
+          featureCode: "PRO"
+        })
       }
     );
 
@@ -81,7 +80,7 @@ export async function POST(request: NextRequest) {
   } catch (error) {
     console.error("Payment initiation error:", error);
     return NextResponse.json(
-      { 
+      {
         error: "Failed to initiate payment",
         details: error instanceof Error ? error.message : "Unknown error"
       },
